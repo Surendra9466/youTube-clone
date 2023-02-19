58a2b2b55305de128ec14965f4e8af0dedb0166a
Surendra Singh <ssingh@forbesadvisor.com>
2023-02-19 11:15:01 +0530

    api call for youtube videos

---
+++ src/components/VideoCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const VideoCard = ({ info }) => {
+    const { snippet, statistics } = info;
+    const { thumbnails, title, channelTitle } = snippet;
+    console.log(info);
+
+    return (
+        <div className='p-2 m-2 w-[280px] shadow'>
+            <img className='rounded-lg' src={thumbnails.medium.url} alt="video" />
+            <ul>
+                <li className='font-bold py-2'>{title}</li>
+                <li>{channelTitle}</li>
+                <li>{statistics.viewCount} views</li>
+            </ul>
+        </div>
+    )
+}
+
+export default VideoCard;
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { YOUTUBE_VIDEOS } from '../utils/contants';
+import VideoCard from './VideoCard';
 
 function VideoContainer() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const res = await fetch(YOUTUBE_VIDEOS);
+    const data = await res.json();
+    setVideos(data.items);
+  }
+  console.log(videos);
+
+
   return (
-    <div>VideoContainer</div>
+    <div className='flex flex-wrap justify-around'>
+      {videos.map((item)=>{
+        return (
+          <VideoCard info={item} key={item.id}/>
+        )
+      })}
+    </div>
   )
 }
 
