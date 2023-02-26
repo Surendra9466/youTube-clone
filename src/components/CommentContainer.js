ea218ec7f96604f08737a7dcfef94cf926f41d46
Surendra Singh <ssingh@forbesadvisor.com>
2023-02-26 12:37:55 +0530

    comment feature

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,9 +1,128 @@
-import React from 'react'
+import React from 'react';
+import Comment from './Comment';
+
+const commentsData = [
+    {
+        name: 'Surendra Singh 1',
+        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+        replies: [
+            {
+                name: 'Surendra Singh nested 1',
+                text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                replies: [
+                    {
+                        name: 'Surendra Singh nested 2',
+                        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                        replies: [
+                            {
+                                name: 'Surendra Singh nested 3',
+                                text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                                replies: [
+                                    {
+                                        name: 'Surendra Singh nested 4',
+                                        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                                        replies: [
+                                            {
+                                                name: 'Surendra Singh nested 5',
+                                                text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                                                replies: [
+                        
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Surendra Singh 2',
+        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+        replies: [
+            {
+                name: 'Surendra Singh nested 2',
+                text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                replies: [
+
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Surendra Singh 3',
+        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+        replies: [
+            {
+                name: 'Surendra Singh nested 2',
+                text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                replies: [
+                    {
+                        name: 'Surendra Singh nested 2',
+                        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                        replies: [
+
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Surendra Singh 4',
+        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+        replies: [
+
+        ]
+    },
+    {
+        name: 'Surendra Singh 5',
+        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+        replies: [
+
+        ]
+    },
+    {
+        name: 'Surendra Singh 6',
+        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+        replies: [
+            {
+                name: 'Surendra Singh nested 2',
+                text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                replies: [
+                    {
+                        name: 'Surendra Singh nested 2',
+                        text: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document',
+                        replies: [
+
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+const CommentList = ({ comments }) => {
+    return comments.map((comment, index) => (
+        <div key={index} >
+            <Comment data={comment} />
+            <div className='pl-5 border border-l-black ml-5'>
+                <CommentList comments={comment.replies} /> 
+            </div>
+        </div>
+    ))
+}
 
 const CommentContainer = () => {
-  return (
-    <div>CommentContainer</div>
-  )
+    return (
+        <div className='m-5 p-2'>
+            <CommentList comments={commentsData} />
+        </div>
+    )
 }
 
 export default CommentContainer;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
+import CommentContainer from './CommentContainer';
 
 const WatchPage = () => {
     const dispatch = useDispatch();
@@ -13,8 +14,11 @@ const WatchPage = () => {
 
 
     return (
-        <div className='px-5'>
-            <iframe width="900" height="500" src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <div>
+            <div className='px-5'>
+                <iframe className='w-full' height="500" src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            </div>
+            <CommentContainer />
         </div>
     )
 }
